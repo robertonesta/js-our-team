742b63988c83fabb379eb4f8d082bf0272a1574d
RobertoNesta <robertonesta1993@gmail.com>
2023-03-22 14:55:52 +0100

    add milestone #1

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+/*Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.  Ogni membro è caratterizzato dalle seguenti informazioni:
+nome
+ruolo
+foto
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede*/
+
+//MILESTONE 1 - CREAZIONE ARRAY DI OGGETTI
+
+const team = [
+
+    {
+        name:"Wayne Barrett",
+        role:"Founder & CEO",
+        pic:""
+    },
+    {
+        name:"Angela Caroll",
+        role:"Chief Editor",
+        pic:""
+    },
+    {
+        name:"Walter Gordon",
+        role:"Office Manager",
+        pic:""
+    },
+    {
+        name:"Angela Lopez",
+        role:"Social Media Manager",
+        pic:""
+    },
+    {
+        name:"Scott Estrada",
+        role:"Developer",
+        pic:""
+    },
+    {
+        name:"Barbara Ramos",
+        role:"Graphic Designer",
+        pic:""
+    }
+]
+//MILESTONE 2 - STAMPO SU CONSOLE TUTTI I MEMBRI DEL TEAM
