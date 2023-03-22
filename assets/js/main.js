78fe60316c2a20ebb7b8197618f015fc120e97fe
RobertoNesta <robertonesta1993@gmail.com>
2023-03-22 15:34:20 +0100

    ADDING BOTH MILESTONE #2 AND BONUSES

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,43 +18,58 @@ Organizzare i singoli membri in card/schede*/
 const team = [
 
     {
-        name:"Wayne Barrett",
-        role:"Founder & CEO",
-        pic:"./assets/img/wayne-barnett-founder-ceo.jpg"
+        name: "Wayne Barrett",
+        role: "Founder & CEO",
+        pic: "wayne-barnett-founder-ceo.jpg"
     },
     {
-        name:"Angela Caroll",
-        role:"Chief Editor",
-        pic:"./assets/img/angela-caroll-chief-editor.jpg"
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        pic: "angela-caroll-chief-editor.jpg"
     },
     {
-        name:"Walter Gordon",
-        role:"Office Manager",
-        pic:"./assets/img/walter-gordon-office-manager.jpg"
+        name: "Walter Gordon",
+        role: "Office Manager",
+        pic: "walter-gordon-office-manager.jpg"
     },
     {
-        name:"Angela Lopez",
-        role:"Social Media Manager",
-        pic:"./assets/img/angela-lopez-social-media-manager.jpg"
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        pic: "angela-lopez-social-media-manager.jpg"
     },
     {
-        name:"Scott Estrada",
-        role:"Developer",
-        pic:"./assets/img/scott-estrada-developer.jpg"
+        name: "Scott Estrada",
+        role: "Developer",
+        pic: "scott-estrada-developer.jpg"
     },
     {
-        name:"Barbara Ramos",
-        role:"Graphic Designer",
-        pic:"./assets/img/barbara-ramos-graphic-designer.jpg"
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        pic: "barbara-ramos-graphic-designer.jpg"
     }
 ]
 //MILESTONE 1 - STAMPO SU CONSOLE TUTTI I MEMBRI DEL TEAM
 //console.log(team);
 
-for (i = 0; i < team.length; i++){
+for (i = 0; i < team.length; i++) {
     const team_member = team[i];
     //console.log(team_member);
     console.log(`name: ${team_member.name}`);
     console.log(`role: ${team_member.role}`);
     console.log(`pic: ${team_member.pic}`);
+    
+    //MILESTONE 2 - Stampare l'array sottoforma di stringhe nel DOM
+    const card = 
+    `<div class="col-4">
+        <div class="card">
+            <h3 class="name">${team_member.name}</h3>
+            <img src="./assets/img/${team_member.pic}" alt="">
+            <p class="role">${team_member.role}</p>
+        </div>
+    </div>`
+
+    const rowElement = document.querySelector(".row")
+    rowElement.innerHTML += card;
 }
+
+
