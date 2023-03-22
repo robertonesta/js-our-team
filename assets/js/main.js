5f7ce01b3b34886a7d2dc9672a0111eabde775e9
RobertoNesta <robertonesta1993@gmail.com>
2023-03-22 15:16:42 +0100

    MILESTONE #0 AND #1 COMPLETE

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,39 +13,48 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede*/
 
-//MILESTONE 1 - CREAZIONE ARRAY DI OGGETTI
+//MILESTONE 0 - CREAZIONE ARRAY DI OGGETTI
 
 const team = [
 
     {
         name:"Wayne Barrett",
         role:"Founder & CEO",
-        pic:""
+        pic:"./assets/img/wayne-barnett-founder-ceo.jpg"
     },
     {
         name:"Angela Caroll",
         role:"Chief Editor",
-        pic:""
+        pic:"./assets/img/angela-caroll-chief-editor.jpg"
     },
     {
         name:"Walter Gordon",
         role:"Office Manager",
-        pic:""
+        pic:"./assets/img/walter-gordon-office-manager.jpg"
     },
     {
         name:"Angela Lopez",
         role:"Social Media Manager",
-        pic:""
+        pic:"./assets/img/angela-lopez-social-media-manager.jpg"
     },
     {
         name:"Scott Estrada",
         role:"Developer",
-        pic:""
+        pic:"./assets/img/scott-estrada-developer.jpg"
     },
     {
         name:"Barbara Ramos",
         role:"Graphic Designer",
-        pic:""
+        pic:"./assets/img/barbara-ramos-graphic-designer.jpg"
     }
 ]
-//MILESTONE 2 - STAMPO SU CONSOLE TUTTI I MEMBRI DEL TEAM
+//MILESTONE 1 - STAMPO SU CONSOLE TUTTI I MEMBRI DEL TEAM
+//console.log(team);
+
+for (i = 0; i < team.length; i++){
+    const team_member = team[i];
+    //console.log(team_member);
+    console.log(`name: ${team_member.name}`);
+    console.log(`role: ${team_member.role}`);
+    console.log(`pic: ${team_member.pic}`);
+}
